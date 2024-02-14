b6c33ca9b46636e262cca60fff967cfbd82d5f4e
anton <lalalala040605@gmail.com>
2024-02-15 02:55:35 +0400

    Добавлена вторая функция capitalize_words и дополнен docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,18 @@
 def uppercase_string(input_string):
+    """
+        Принимает на вход строку и возвращает ее со всеми заглавными буквами.
+
+        :param input_string: входная строка
+        :return: строка со всеми заглавными буквами
+        """
     return input_string.upper()
+
+
+def capitalize_words(input_string):
+    """
+    Принимает на вход строку и возвращает ее с заглавными первыми буквами каждого слова.
+
+    :param input_string: входная строка
+    :return: строка с заглавными первыми буквами каждого слова
+    """
+    return input_string.title()
