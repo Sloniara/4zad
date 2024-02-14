53492fd3384447aa7cb54302903c8584d47d0aee
Sloniara <158821614+Sloniara@users.noreply.github.com>
2024-02-15 02:52:49 +0400

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def uppercase_string(input_string):
+    """
+    Принимает на вход строку и возвращает ее со всеми заглавными буквами.
+    
+    :param input_string: входная строка
+    :return: строка со всеми заглавными буквами
+    """
     return input_string.upper()
